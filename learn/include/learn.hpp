66d4d22e661ce2535d60035426084d9c8872f221
dabaicai <dabaicai@gmail.com>
2020-08-27 15:29:57 +0800

    multi_index

--- learn/include/learn.hpp
+++ learn/include/learn.hpp
@@ -14,8 +14,14 @@ CONTRACT learn : public contract {
    public:
       using contract::contract;
 
+      
+
+
+
+
+
       ACTION hi( name nm );
-      ACTION testcheck(){
+      ACTION testcheck1(){
          uint8_t a=1;
          uint8_t b=2;
          //check(a==b,"a !=b");
@@ -44,6 +50,41 @@ CONTRACT learn : public contract {
          print(account, " ");               
          print(age, " ");
       }
+      ACTION testcheck(){
+         user_index users(get_self(),get_first_receiver().value);
+         check(users.get_code()=="zhouhao"_n,"codes do not match!");
+         check(users.get_scope()==name{"zhouhao"}.value,"scopes do not match!");
+         print("match");
+
+      }
+      ACTION adduser(name account,uint64_t phone,std::string signture){
+         require_auth(account);
+         user_index users(get_self(),get_first_receiver().value);
+         users.emplace(account,[&](user& user){
+            user.account =account;
+            user.phone = phone;
+            user.regtime = time_point_sec(current_time_point());
+            user.signature = signture;
+
+         });
+      }
+
+      ACTION addpost(name account,std::string content){
+         require_auth(account);
+         user_index users(get_self(),get_first_receiver().value);
+
+         auto itr = users.find(account.value);
+         eosio::check(itr != users.end(), "account not found");
+
+         post_index posts(get_self(), get_first_receiver().value); 
+         posts.emplace(account, [&](auto& post){
+         post.id = posts.available_primary_key();
+         post.author = account;
+         post.content = content;
+      });
+         
+
+      }
 
 
       //using hi_action = action_wrapper<"hi"_n, &learn::hi>;
@@ -53,8 +94,41 @@ CONTRACT learn : public contract {
 
       USING_ACTION(testcontract);
       USING_ACTION(testlearn);
+      USING_ACTION(testcheck1);
       USING_ACTION(testcheck);
+      USING_ACTION(addpost);
+      USING_ACTION(adduser);
       USING_ACTION(hi);
+
+   private:
+      TABLE user {
+         name account; //使用EOS账户名作为主键
+         uint64_t phone;
+         time_point_sec regtime; 
+         std::string signature;
+         uint64_t primary_key() const { return account.value; }
+         uint64_t get_secondary_1() const { return phone; }
+         uint64_t get_secondary_2() const { return regtime.sec_since_epoch(); }
+      };
+
+      TABLE post {
+      uint64_t id; //使用自增id作为主键
+      name author;
+      std::string content;
+      uint64_t primary_key() const { return id; }
+      uint64_t get_secondary_1() const { return author.value; }
+      };
+
+      typedef multi_index<"user"_n, user, 
+      indexed_by<"byphone"_n,    const_mem_fun<user, uint64_t, &user::get_secondary_1>>,
+      indexed_by<"byregtime"_n,  const_mem_fun<user, uint64_t, &user::get_secondary_2>>
+      > user_index;
+
+      typedef multi_index<"post"_n, post,
+      indexed_by<"byauthor"_n, const_mem_fun<post, uint64_t, &post::get_secondary_1>>
+      > post_index;
+
+
       
 };
 
