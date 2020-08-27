799509f1a10bfd0a0c3f91fd67d911e824bbb369
dabaicai <dabaicai@gmail.com>
2020-08-27 13:50:35 +0800

    define USING_ACYION

--- learn/include/learn.hpp
+++ learn/include/learn.hpp
@@ -1,6 +1,12 @@
 
 #include <eosio/eosio.hpp>
 #include <string>
+#include <vector>
+#include <eosio/system.hpp>
+#define USING_ACTION(name) using name##_action = action_wrapper<#name##_n, &learn::name>
+
+
+
 
 using namespace eosio; 
 
@@ -18,10 +24,38 @@ CONTRACT learn : public contract {
          check(a==b,1);
          std::string name="刘清";
          print(name); //为什么没有输出呢？
+         std::vector<int> v{1,2,3,4,5};
+         for(auto& item:v){
+            print(item,'\n');
+
+         }
+         
+      }
+      ACTION testlearn(){
+         print(seconds(11).count(),'\n');
+         print(current_time_point().sec_since_epoch(), " ");
+         print(current_block_time().to_time_point().sec_since_epoch(), " ");
+
       }
 
-      using hi_action = action_wrapper<"hi"_n, &learn::hi>;
-      using testcheck_action = action_wrapper<"testcheck"_n, &learn::testcheck>;
+      ACTION testcontract(name account, uint64_t age) {
+         print(get_self(), " ");            
+         print(get_first_receiver(), " ");  
+         print(account, " ");               
+         print(age, " ");
+      }
+
+
+      //using hi_action = action_wrapper<"hi"_n, &learn::hi>;
+      //using testcheck_action = action_wrapper<"testcheck"_n, &learn::testcheck>;
+      //using testlearn_action = action_wrapper<"testlearn"_n, &learn::testlearn>;
+      //using testcontract_action = action_wrapper<"testcontract"_n, &learn::testcontract>;
+
+      USING_ACTION(testcontract);
+      USING_ACTION(testlearn);
+      USING_ACTION(testcheck);
+      USING_ACTION(hi);
+      
 };
 
 
